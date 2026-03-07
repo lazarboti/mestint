16a0e8fb19a53eb63e76245cbc49f0403fbdb382
lazarboti <161580615+lazarboti@users.noreply.github.com>
2026-03-07 11:43:39 +0100

    Add files via upload

--- kiralyno.py
+++ kiralyno.py
@@ -1,33 +1,41 @@
+class NKiralyno:
 
-class Kiralyno:
+    def __init__(self, n):
+        self.N = n
+        koordinatak = tuple((0, 0) for i in range(n))
+        self.kezdo = (koordinatak, 1)
 
-    def __init__(self, k , c):
-        self.kezdo = k
-        self.cel = c
-        self.N = len(k) - 1
+    def celteszt(self, a):
+        koordinatak, s = a
+        return s == self.N + 1
 
-    def celteszt(self, a):  # a= (a1,a2,a3....an,s)
-        return a[-1] == self.cel
-
-    def rakovetkezo(self, a): # a= (a1,a2,a3....an,s)
+    def rakovetkezo(self, a):
         gyerekek = []
-        s = a[-1] # következő sor indexe
-        for i in range(1,self.N+1):
-            előfeltétel = True # lerak (s,i) alkalmazható????
-            for m in range(1,s): #bármely m < s:
-                if a[m-1] != i and abs(s-m) != abs(i -a[m-1]):
-                    pass
-                else:
-                    előfeltétel = False
-                    break
+        koordinatak, s = a
+        if s > self.N:
+            return gyerekek
+        for i in range(1, self.N + 1):
+            utkozik = False
+            sor_most = s - 1
+            oszlop_most = i
 
-            if előfeltétel:
-                uj_allapot = list(a)
-                uj_allapot[s-1] = i
-                uj_allapot[-1] = s+1
-                gyerekek.append(tuple(uj_allapot))
+            for j in range(1, s):
+                sor_regi, oszlop_regi = koordinatak[j - 1]
+                if oszlop_regi == oszlop_most or abs(sor_regi - sor_most) == abs(oszlop_regi - oszlop_most):
+                    utkozik = True
+                    break
+            if not utkozik:
+                uj_koordinatak = list(koordinatak)
+                uj_koordinatak[s - 1] = (sor_most, oszlop_most)
+                uj_allapot = (tuple(uj_koordinatak), s + 1)
+                gyerekek.append(uj_allapot)
+
         return gyerekek
 
 if __name__ == '__main__':
-    feladat = Kiralyno((0,0,0,0,1), 5)
-    feladat2 = Kiralyno((0,0,0,0,0,0,0,0,0,1),9)
+    feladat = NKiralyno(4)
+    print(f"Kezdőállapot: {feladat.kezdo}")
+    elso_lepesek = feladat.rakovetkezo(feladat.kezdo)
+    print(f"Lehetséges első lépések száma: {len(elso_lepesek)}")
+    for allapot in elso_lepesek:
+        print(allapot)
